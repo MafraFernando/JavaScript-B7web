44457b2ac5eb2af3f37e7598673a78a2c307c23c
MafraFernando <145868730+MafraFernando@users.noreply.github.com>
2024-06-21 17:48:04 -0300

    atualizado até a aula 10

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-console.log("Olá, mundo 3.0")
+let nome = "Fernando" //String
+let idade = 32 // Number
+let logado = true // Boolean
+let lista = ["ovo, farinha, leite"] //Arrey
+let nomeCompleto = { nome: "Fernando", idade: 32 } // Object
+
+// O comando abaixo exibe os dados no console
+// console.log(typeof logado)
+
+/* EXERCÍCIOS
+1. Crie uma variável para armazenar o nome do carro e coloque o valor Ferrari. e exiba-a.
+
+2. Crie uma variável para armazenar o preço do bolo, que custa R$ 19,99. e exiba-a.
+
+3. Corrija o código a seguir e exiba-o.
+let "cidade" = São Paulo
+*/
+
+// exercicio 1
+
+let carro = "ferrari"
+
+console.log(carro)
+
+// exercicio 2
+
+let preco = 19.99
+
+console.log(preco)
+
+// exercicio 3
+
+let cidade = "São Paulo"
+
+console.log(cidade)
+
+/* parei na aula 10, fala sobre var, let e const sendo duas variáveis e uma constante, a constante não pode ser alterada, já as variáveis podem variar. */
